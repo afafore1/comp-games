d51102cb698a61fe2337e3759c2fb31c462c7e1d
Ayomitunde Fafore <ayo@streamlit.io>
2023-12-23 20:15:15 +0000

    initial commit

--- Hello.py
+++ Hello.py
@@ -1,51 +1,28 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import requests
+import pandas as pd
 
-LOGGER = get_logger(__name__)
+# Function to fetch games from RAWG API
+@st.cache_data
+def fetch_games(genre):
+    api_key = st.secrets['api_key']
+    url = f"https://api.rawg.io/api/games?key=api_key&genres={genre}"
+    response = requests.get(url)
+    return response.json()
 
+# Streamlit app layout
+st.title('Competitive Video Games Dashboard')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Select genre
+genre = st.selectbox("Select a Game Genre:", ['action', 'strategy', 'shooter', 'sports'])
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+# Fetch and display games
+data = fetch_games(genre)
+if 'results' in data:
+    games = data['results']
+    for game in games:
+        st.subheader(game['name'])
+        st.write(f"Released: {game['released']}")
+        st.write(f"Rating: {game['rating']}/5")
+        if game['background_image']:
+            st.image(game['background_image'])
